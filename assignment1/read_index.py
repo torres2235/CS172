a27621c380f331f21b7a594722719a541058c409
torres2235 <43655957+torres2235@users.noreply.github.com>
2021-05-21 17:47:57 -0700

    Update read_index.py

--- assignment1/read_index.py
+++ assignment1/read_index.py
@@ -1,1 +1,11 @@
 # This file should contain code to receive either a document-id or word or both and output the required metrics. See the assignment description for more detail.
+import sys
+
+if (sys.argv[1] == "--doc" and len(sys.argv) == 3):
+    #print("this is a document")
+    print("Listing for document: " + sys.argv[2])
+elif (sys.argv[1] == "--term" and len(sys.argv) == 3):
+    #print("this is a term")
+    print("Listing for term: " + sys.argv[2])
+else:
+    print("ERROR: must provide a term or document")
