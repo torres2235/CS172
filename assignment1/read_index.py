5f3197e8961004a705e69cca2a702ab5b35ee6e1
torres2235 <43655957+torres2235@users.noreply.github.com>
2021-05-21 18:24:55 -0700

    Update read_index.py

--- assignment1/read_index.py
+++ assignment1/read_index.py
@@ -1,5 +1,6 @@
 # This file should contain code to receive either a document-id or word or both and output the required metrics. See the assignment description for more detail.
 import sys
+import re
 
 if (sys.argv[1] == "--doc" and len(sys.argv) == 3):
     #print("this is a document")
@@ -7,5 +8,18 @@ if (sys.argv[1] == "--doc" and len(sys.argv) == 3):
 elif (sys.argv[1] == "--term" and len(sys.argv) == 3):
     #print("this is a term")
     print("Listing for term: " + sys.argv[2])
+
+    termids_file = "termids.txt"
+
+    with open(termids_file) as f:
+        content = f.read().splitlines()
+
+    for line in content:
+        new_line = re.sub(r'\d+''\t+','',line) #remove the termid and \t so we can compare it to the term we are trying to find
+        #print(new_line)
+        if new_line == sys.argv[2]:
+            new_line = re.sub(r"[a-z]",'',line)
+            #print(line)
+            print("TERMID: " + new_line)
 else:
     print("ERROR: must provide a term or document")
