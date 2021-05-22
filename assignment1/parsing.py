76e8b893e07966842eb1fe4366804fd34a279468
torres2235 <43655957+torres2235@users.noreply.github.com>
2021-05-21 17:48:24 -0700

    Update parsing.py

--- assignment1/parsing.py
+++ assignment1/parsing.py
@@ -16,7 +16,7 @@ with zipfile.ZipFile("ap89_collection_small.zip", 'r') as zip_ref:
 for dir_path, dir_names, file_names in os.walk("ap89_collection_small"):
     allfiles = [os.path.join(dir_path, filename).replace("\\", "/") for filename in file_names if (filename != "readme" and filename != ".DS_Store")]
 
-#-----------------------Generating my stopwords here--------------------------#
+#----------------------- Generating my stopwords here --------------------------#
 
 stopwords_list = []
 with open('stopwords.txt', 'r') as stopwords:
@@ -24,14 +24,32 @@ with open('stopwords.txt', 'r') as stopwords:
         line = re.sub('\n',"", line)
         #print(line)
         stopwords_list.append(str(line))
+stopwords.close()
 #print stopwords_list
+#-------------------------------------------------------------------------------#
 
+#--------------------- Creating some Globals -----------------------------------#
 words = [] # holds the words in our doc without stopwords
-tokens = [] # holds our tuples
-terms = [] # holds our terms and the index for them is our TermId(?)
-doc_num = [] # holds our docnos, the index for them is our DocId(?)
-#--------------------------------------------------------------------#
+#tokens = [] # holds our tuples
 
+termindex = 1
+docindex = 1
+TermIds = dict() # holds our terms and TermIds
+DocIds = dict() # holds our docnos and DocIds
+posting_dictionary = dict() # comnbines our TermIds and DocIds and uses our terms as keys
+#-------------------------------------------------------------------------------#
+
+#--------------------------------.txt prep -------------------------------------#
+docids_file = open("docids.txt", "w")
+docids_file.write("") # reset our file
+docids_file.close()
+docids_file = open("docids.txt", "a") # put our file into append mode
+
+termids_file = open("termids.txt", "w")
+termids_file.write("") # reset our file
+termids_file.close()
+termids_file = open("termids.txt", "a") # put our file into append mode
+#-------------------------------------------------------------------------------#
 for file in allfiles:
     with open(file, 'r', encoding='ISO-8859-1') as f:
         filedata = f.read()
@@ -58,29 +76,25 @@ for file in allfiles:
             #print(words)
 
 #-------------------------- step 2 - create tokens --------------------------#
-            if docno not in doc_num:
-                doc_num.append(docno)
-            #print doc_num.index(docno)
             
-            for word in words:
-                if word not in terms:
-                    terms.append(word) # add word into terms if it is unique
-                tmp = (terms.index(word) + 1, "DocID: " + str(doc_num.index(docno) + 1), words.index(word) + 1)
-                if tmp not in tokens:
-                    tokens.append(tmp) # create the tuple
-            #print(tokens)
-            #print terms
-#-------------------------- step 3 - build index --------------------------#
-TermId = []
-DocId = []
+            if docno not in DocIds:
+                DocIds[docno] = str(docindex)
+                docids_file.write(str(docindex) + "\t" + docno + "\n") # add docid and docno into our .txt
+            #docids_file.close()
+            #print(DocIds)
 
-for word in terms:
-    tmp = (terms.index(word) + "  " + word)
-    TermId.append(tmp)
-    
-for doc in doc_num:
-    tmp = (doc_num.index(doc) + "  " + doc)
-    DocId.append(tmp)
-    
-print TermId
-print DocId
+            for index, word in enumerate(words):
+                if word not in TermIds:
+                    TermIds[word] = termindex # add word into terms if it is unique
+                    termids_file.write(str(termindex) + "\t" + word + "\n") # add termid and term into our .txt
+                    posting_dictionary[word] = [] # make a new space in our posting_dictionary so we can append to it
+                    termindex += 1
+
+                tmp = (TermIds[word], DocIds[docno], index + 1)
+                if TermIds[word] not in posting_dictionary:   
+                    posting_dictionary[word].append(tmp) # create the tuple
+
+            docindex += 1
+            #print(DocIds[docno])
+print(posting_dictionary)
+#-------------------------- step 3 - build index --------------------------#
